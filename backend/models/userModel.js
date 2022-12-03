00405ad3890338bc8fc8c3dff130c0e6c52effd9
Udyan <udyanojha@gmail.com>
2022-12-03 20:33:48 +0530

    Initial Commit

---
+++ backend/models/userModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema(
+    {
+        name: {type: String, required: true},
+        email: {type: String, required: true, unique: true},
+        password: {type: String, required: true},
+        pic: {type: String, default: '/backend/extras/user.jpg'},
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
